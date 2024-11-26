1903d977bca89c829fb6e59fa4edb02fc00289ce
Johannes <johannestammerand@gmail.com>
2024-11-26 20:41:20 +0200

    improved footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,9 @@
 <template>
     <footer>
-      <p>WAD Homework #3</p>
+      <p>This page was made by group E</p>
+      <p>Content on this site is licensed under absolutely nothing</p>
+      <p>You will not get any contact info</p>
+      <p>There are no terms of use, go wild</p>
     </footer>
   </template>
   
@@ -14,15 +17,14 @@
 footer {
   background-color: lightgray;
   text-align: center;
-  padding: 0px;
-  position: fixed;
+  padding: 10px;
   left: 0;
   bottom: 10px;
   width: calc(100% - 20px);
   margin: 0 10px; 
   box-sizing: border-box;
   border-radius: 10px;
-  font-size: 18px;
+  font-size: 14px;
   font-weight: bold;
 }
 
